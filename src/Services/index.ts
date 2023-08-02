6d5769b3dd6278797a2e37204b8f30868545c23b
Sanskriti Saluja <sanskriti.saluja98@gmail.com>
2023-08-02 20:01:33 +0530

    fix:key change

--- src/Services/index.ts
+++ src/Services/index.ts
@@ -2,13 +2,13 @@ import { ISearchList, IStockDetail } from "types";
 
 import { Get } from "utils/Request";
 
+const key = "SHRQHGPDBPFOJKPJ";
+
 export const GetSearchSymbol = async (keyword: string): Promise<ISearchList> =>
-  Get(
-    `query?function=SYMBOL_SEARCH&keywords=${keyword}&apikey=SHRQHGPDBPFOJKPJ`
-  );
+  Get(`query?function=SYMBOL_SEARCH&keywords=${keyword}&apikey=${key}`);
 
 export const GetStockDetails = async (
   _refresh: boolean,
   payload: string
 ): Promise<IStockDetail> =>
-  Get(`query?function=OVERVIEW&symbol=${payload}&apikey=IAP8S08I17VAF8HB`);
+  Get(`query?function=OVERVIEW&symbol=${payload}&apikey=${key}`);
